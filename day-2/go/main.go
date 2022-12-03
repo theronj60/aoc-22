c7bd189b3212e7070233b2971488321b9b264c8d
Theron Joe <theronj@joetwebdev.io>
2022-12-03 00:17:34 -0700

    completed part one, starting part two

--- day-2/go/main.go
+++ day-2/go/main.go
@@ -16,6 +16,17 @@ import (
 // Y => paper
 // Z => scissor
 
+// var total int
+// total = 6
+// scoring based on shape
+// rock = 1
+// paper = 2
+// scissors = 3
+// scoring based on outcome
+// loss = 0
+// draw = 3
+// win = 6
+
 func getHand(hand string) string {
 	var gesture string
 	switch hand {
@@ -30,27 +41,54 @@ func getHand(hand string) string {
 }
 
 func main() {
-	content, err := ioutil.ReadFile("../rps.test")
-	// content, err := ioutil.ReadFile("../rps.prod")
+	// content, err := ioutil.ReadFile("../rps.test")
+	content, err := ioutil.ReadFile("../rps.prod")
 	if err != nil {
 		fmt.Println("Err")
 	}
 
+	split := strings.Split(string(content), "\n")
+
+	var opponent string
+	var player string
+	var score int
 	var total int
 
-	// scoring based on shape
-	// rock = 1
-	// paper = 2
-	// scissors = 3
-	// scoring based on outcome
-	// loss = 0
-	// draw = 3
-	// win = 6
-
-	split := strings.Split(string(content), "\n")
 	for _, item := range split {
-		// second item in slice is a space
-		// compare item[0] (opponent) to item[2] (player)
-		fmt.Println(string(item[1]))
+		// below if addresses an empty last item
+		if item == split[len(split)-1] {
+			break
+		}
+		opponent = getHand(string(item[0]))
+		player = getHand(string(item[2]))
+		switch player {
+		case "rock":
+			score = 1
+		case "paper":
+			score = 2
+		case "scissor":
+			score = 3
+		}
+		if opponent == "rock" {
+			if player == "rock" {
+				score += 3
+			} else if player == "paper" {
+				score += 6
+			}
+		} else if opponent == "paper" {
+			if player == "paper" {
+				score += 3
+			} else if player == "scissor" {
+				score += 6
+			}
+		} else if opponent == "scissor" {
+			if player == "scissor" {
+				score += 3
+			} else if player == "rock" {
+				score += 6
+			}
+		}
+		total += score
 	}
+	fmt.Println(total)
 }
