e6836f8014130c1adf7c7bba9bcf0112212ff7a6
Theron Joe <theronj@joetwebdev.io>
2022-12-06 12:37:51 -0700

    completed day 3, moving on to day 4

--- day-3/go/main.go
+++ day-3/go/main.go
@@ -10,6 +10,57 @@ var alphabet = [...]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
 	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
+func partTwo(split []string) {
+	var sumTwo int
+	var group int
+	var numGroup []string
+
+	for _, value := range split {
+		var letter string
+		numGroup = append(numGroup, value)
+		group++
+		
+		if group == 3 {
+			letter = compareMore(numGroup) // what should i pass here
+			sumTwo += getInt(letter)
+			numGroup = nil
+			group = 0
+		}
+	}
+	fmt.Println("sum Part Two: ", sumTwo)
+}
+
+func compareMore(group []string) string {
+	var setter string
+
+	for _, letterInFirst := range group[0] {
+		for index, value := range group {
+			if index == 1 {
+				for _, letter := range value {
+					if string(letter) == string(letterInFirst) {
+						setter = string(letter)
+					}
+					continue
+				}
+			}
+			if index == 2 {
+				for _, letter := range value {
+					if string(letterInFirst) == string(letter) {
+						if string(letter) == setter {
+							setter = string(letter)
+							return setter
+						} else {
+							setter = ""
+							continue
+						}
+					} 
+				}
+			}
+		}
+	}
+	return setter
+}
+
 func compare(word string, value string) bool {
 	for i, letter := range word {
 		if i > len(word)/2 - 1 {
@@ -33,8 +84,8 @@ func getInt(value string) int {
 }
 
 func main() {
-	content, err := ioutil.ReadFile("../ruck.test")
-	// content, err := ioutil.ReadFile("../ruck.prod")
+	// content, err := ioutil.ReadFile("../ruck.test")
+	content, err := ioutil.ReadFile("../ruck.prod")
 	if err != nil {
 		fmt.Println("Err")
 	}
@@ -52,8 +103,8 @@ func main() {
 				}
 			}
 		}
-		fmt.Println(letter)
 		sum += getInt(letter)
 	}
 	fmt.Println("sum part one: ", sum)
+	partTwo(split)
 }
